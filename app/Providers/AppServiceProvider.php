0c3d1afc896c7277ef115eae8a4f739e23e5ab7c
hanifpham <haniflks.2023@gmail.com>
2025-12-23 00:13:28 +0700

    Force HTTPS schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- Jangan lupa baris ini!
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Paksa HTTPS jika aplikasi sudah online (Production)
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
