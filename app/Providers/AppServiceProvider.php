4330584439677e9aacbc404acca0aef6de9570b2
hanifpham <haniflks.2023@gmail.com>
2025-12-23 01:24:50 +0700

    Fix Trust Proxies for HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- Jangan lupa baris ini!
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Paksa HTTPS jika request/proxy memakai HTTPS atau APP_URL sudah HTTPS.
+        $appUrl = config('app.url');
+        $forwardedProto = request()->header('x-forwarded-proto');
+        $shouldForceHttps = false;
+
+        if (is_string($appUrl) && str_starts_with($appUrl, 'https://')) {
+            $shouldForceHttps = true;
+        }
+
+        if (is_string($forwardedProto) && str_contains($forwardedProto, 'https')) {
+            $shouldForceHttps = true;
+        }
+
+        if ($shouldForceHttps) {
+            URL::forceScheme('https');
+        }
     }
 }
